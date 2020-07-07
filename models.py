9ee8b16f88637103ad6364d9213b5ce86916d1a3
turaboyh <turabekh@gmail.com>
2020-07-07 18:49:47 -0400

    added db.drop_all()

--- models.py
+++ models.py
@@ -20,6 +20,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+    db.drop_all()
     migrate = Migrate(app, db)
     db.create_all()
 
